74606430e3ded9a2284a8a6dd14e13d37bf8a845
xxmark10xx <mark.a.aguilar99@gmail.com>
2022-02-25 09:55:06 -0800

    complete users db

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const express = require('express')
 const app = express()
+const ejsLayouts = require('express-ejs-layouts')
+require('dotenv').config()
+
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+})
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+    console.log(`Auth app running on ${PORT}`)
+})
